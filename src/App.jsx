ada32ad68fa802c239d12592ca262e0b52cb5ec1
Michael Boadum Ahiale <abmike268@gmail.com>
2024-04-14 22:22:24 +0000

    Resolved App.jsx missing components issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/contact/Contact";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Projects/>
+    <Contact/>
+  </div>;
 };
 
 export default App;
