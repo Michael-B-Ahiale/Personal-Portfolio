3ec641b377757bd0e303564d80192cf53c9d8ce6
Michael Boadum Ahiale <abmike268@gmail.com>
2024-04-17 10:56:35 +0000

    Framer motion removed from Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,18 @@
 import Sidebar from '../Sidebar/Sidebar'
 import './navbar.scss'
-import { motion } from 'framer-motion';
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
     <Sidebar/>
       <div className="wrapper">
-        <motion.span className='logo'
-        initial={{opacity:0,scale:0.5}}
-        animate={{opacity:1,scale:1}}
-        transition={{duration:0.5}}
-        >{`M<>A`}</motion.span>
+        <span className='logo'
+        >{`M<>A`}</span>
         <div className="links">
           
-          <motion.a whileHover={{scale:1.15}} whileTap={{scale:0.95}} href="#Skills">LinkedIn</motion.a>
-          <motion.a whileHover={{scale:1.15}} whileTap={{scale:0.95}} href="#Projects">Github</motion.a>
+          <a href="#Skills">LinkedIn</a>
+          <a href="#Projects">Github</a>
           
         </div>
       </div>
